29f16c915bbd7e25eff7019f185fcdf6b87b2b40
Hugo Wong-Berard <hugowong71@gmail.com>
2020-06-26 21:11:51 +0100

    Updates

---
+++ precache-manifest.afe8ff096eb760e098350fb62a60e9ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79615f5f7a604c3a33b829c8981c1f0a",
+    "url": "/linear-regression-visualization/index.html"
+  },
+  {
+    "revision": "ee77f603e114cbf8f76d",
+    "url": "/linear-regression-visualization/static/css/main.7bf9ee29.chunk.css"
+  },
+  {
+    "revision": "886498150cfcda738555",
+    "url": "/linear-regression-visualization/static/js/2.202fe975.chunk.js"
+  },
+  {
+    "revision": "57212f8b5acde66944bbcd294e9700c1",
+    "url": "/linear-regression-visualization/static/js/2.202fe975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee77f603e114cbf8f76d",
+    "url": "/linear-regression-visualization/static/js/main.384785f7.chunk.js"
+  },
+  {
+    "revision": "5bdee7fdd9f81728b00f",
+    "url": "/linear-regression-visualization/static/js/runtime-main.c1243ad2.js"
+  }
+]);
